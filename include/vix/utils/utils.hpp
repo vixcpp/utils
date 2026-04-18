e93f3482f2254d61e524788e8461088ccaa340b8
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-04-18 16:48:15 +0300

    feat(utils): add umbrella header

---
+++ include/vix/utils/utils.hpp
@@ -0,0 +1,39 @@
+/**
+ *
+ *  @file utils/utils.hpp
+ *  @author Gaspard Kirira
+ *
+ *  @brief Internal aggregation header for the Vix utils module.
+ *
+ *  This file includes the common utility components provided by Vix,
+ *  including environment helpers, logging, string utilities, time,
+ *  validation, UUID generation, and supporting helpers.
+ *
+ *  For most use cases, prefer:
+ *    #include <vix/utils.hpp>
+ *
+ *  Copyright 2025, Gaspard Kirira.
+ *  All rights reserved.
+ *  https://github.com/vixcpp/vix
+ *
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  Vix.cpp
+ */
+#ifndef VIX_UTILS_UTILS_HPP
+#define VIX_UTILS_UTILS_HPP
+
+#include <vix/utils/ConsoleMutex.hpp>
+#include <vix/utils/Env.hpp>
+#include <vix/utils/Logger.hpp>
+#include <vix/utils/Result.hpp>
+#include <vix/utils/ScopeGuard.hpp>
+#include <vix/utils/ServerPrettyLogs.hpp>
+#include <vix/utils/String.hpp>
+#include <vix/utils/Time.hpp>
+#include <vix/utils/UUID.hpp>
+#include <vix/utils/Validation.hpp>
+#include <vix/utils/Version.hpp>
+
+#endif // VIX_UTILS_UTILS_HPP
