083df9b78110d88aa0e6157f6e31ae4220a079f8
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-08-18 21:11:00 +0300

    test(utils): avoid spdlog macro false positive

--- tests/spdlog_interface_regression.cpp
+++ tests/spdlog_interface_regression.cpp
@@ -1,8 +1,11 @@
-#include <spdlog/spdlog.h>
-
+// Check the compiler command line before spdlog's own configuration headers
+// have a chance to define this macro.  Packaged spdlog installations may set
+// SPDLOG_FMT_EXTERNAL in tweakme.h; that is not a vix::utils interface leak.
 #ifdef SPDLOG_FMT_EXTERNAL
 #error "vix::utils leaked SPDLOG_FMT_EXTERNAL into an independent consumer"
 #endif
+
+#include <spdlog/spdlog.h>
 
 int main()
 {
